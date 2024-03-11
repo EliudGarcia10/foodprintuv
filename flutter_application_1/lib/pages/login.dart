729a0b61b5c1522cd7e6665f982ec95c7f81f186
EliudGarcia11 <garciapazos61@gmail.com>
2024-03-11 13:14:07 -0600

    primer comit eliud

---
+++ flutter_application_1/lib/pages/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget{
+  const LoginPage({super.key});
+
+
+  @override
+  State<LoginPage> createState() => _LoginPage();
+
+  
+}
+
+class _LoginPage extends State<LoginPage> {
+   @override
+  Widget build(BuildContext context) {
+    return(Scaffold(appBar: null, body: Container(),));
+  }
+}
